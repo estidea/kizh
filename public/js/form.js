ea8acd0064b0b6cb54538d1cd81245e9d23d8525
estidea <whatsoever@bk.ru>
2018-09-20 16:59:13 +0300

    fix: iOS scrolling, textarea autoresize, ending of the white era

--- public/js/form.js
+++ public/js/form.js
@@ -1,23 +1,38 @@
 $(document).ready(function () {
+  autosize($('textarea'));
+  // var aniLogo = $("#contact-minilogo");
+  // aniLogo.css("transition", "all 19.9s linear");
+  // aniLogo.css("transform", "translateX(200%)");
+  // var x = '200%';
   // setInterval(function() {
-  //   left++;
-  //   $('#contact-logo').css('left',left+'px');
-  // },500);
+  //   if (x == '200%') {
+  //     x = '-100%';
+  //     aniLogo.css("transition", "all 0s linear");
+  //     aniLogo.css("transform", "translateX("+ x +")");
+  //     setTimeout(()=>{
+  //       x = '200%';
+  //       aniLogo.css("transition", "all 19.9s linear");
+  //       aniLogo.css("transform", "translateX("+ x +")");
+  //     },100)
+  //   }
+  // },20000);
+
+
   /* Elastic textarea */
-  var idNum = 0, data = 'elastic'; 
-  $('body').on('keyup', 'textarea[data^="'+data+'"]', function(){ 
-    if($(this).attr('data')==''+data+''){$(this).attr({style:'overflow:hidden;'+$(this).attr('style')+'',data:''+$(this).attr('data')+''+idNum+''});idNum++;} 
-    tData = $(this).attr('data'); 
-    if($('div[data="'+tData.replace(''+data+'','clone')+'"]').size()==0){ 
-    attr = 'style="display:none;padding:'+$(this).css('padding')+';width:'+$(this).css('width')+';min-height:'+$(this).css('height')+';font-size:'+$(this).css('font-size')+';line-height:'+$(this).css('line-height')+';font-family:'+$(this).css('font-family')+';white-space:'+$(this).css('white-space')+';word-wrap:'+$(this).css('word-wrap')+';letter-spacing:0.2px;" data="'+tData.replace(''+data+'','clone')+'"'; 
-    clone = '<div '+attr+'>'+$(this).val()+'</div>'; 
-    $('body').prepend(clone); 
-    idNum++; 
-    }else{ 
-    $('div[data="'+tData.replace(''+data+'','clone')+'"]').html($(this).val()); 
-    $(this).css('height',''+$('div[data="'+tData.replace(''+data+'','clone')+'"]').css('height')+''); 
-    } 
-  });
+  // var idNum = 0, data = 'elastic'; 
+  // $('body').on('keyup', 'textarea[data^="'+data+'"]', function(){ 
+  //   if($(this).attr('data')==''+data+''){$(this).attr({style:'overflow:hidden;'+$(this).attr('style')+'',data:''+$(this).attr('data')+''+idNum+''});idNum++;} 
+  //   tData = $(this).attr('data'); 
+  //   if($('div[data="'+tData.replace(''+data+'','clone')+'"]').size()==0){ 
+  //   attr = 'style="display:none;padding:'+$(this).css('padding')+';width:'+$(this).css('width')+';min-height:'+$(this).css('height')+';font-size:'+$(this).css('font-size')+';line-height:'+$(this).css('line-height')+';font-family:'+$(this).css('font-family')+';white-space:'+$(this).css('white-space')+';word-wrap:'+$(this).css('word-wrap')+';letter-spacing:0.2px;" data="'+tData.replace(''+data+'','clone')+'"'; 
+  //   clone = '<div '+attr+'>'+$(this).val()+'</div>'; 
+  //   $('body').prepend(clone); 
+  //   idNum++; 
+  //   }else{ 
+  //   $('div[data="'+tData.replace(''+data+'','clone')+'"]').html($(this).val()); 
+  //   $(this).css('height',''+$('div[data="'+tData.replace(''+data+'','clone')+'"]').css('height')+''); 
+  //   } 
+  // });
 
   /* Material Input */
   $(".form-input, .form-textarea").focus(function(){
